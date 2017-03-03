0f52ff9e571194faf7be62d8e9a6ceb7765a1503
Ondrej Oravcok <ondrej.oravcok@unicornsystems.eu>
2017-03-03 23:58:37 +0100

    create/update Place with Remove - implemented

--- front-end/src/main/webapp/elements/places/createPlace.js
+++ front-end/src/main/webapp/elements/places/createPlace.js
@@ -1,25 +1,92 @@
 'use strict';
 
 angular.module('angularApp')
-    .controller('CreatePlaceCtrl', ['$scope', '$http', 'commonTools', function ($scope, $http, commonTools) {
+    .controller('CreatePlaceCtrl', ['$scope', '$http', '$location', 'commonTools', 'createUpdateTools', function ($scope, $http, $location, commonTools, createUpdateTools) {
         $scope.master = {};
+        $scope.doing = 'create';
+        if (createUpdateTools.getItem() != null) {
+            $scope.place = angular.copy(createUpdateTools.getItem());
+            $scope.doing = 'update';
+        }
 
         $scope.update = function(place) {
-            $scope.status = "";
             $scope.master = angular.copy(place);
-            $http({
-                url: '/posta/rest/places',
-                method: "POST",
-                data: $scope.master
-            }).then(function (response) {
-                $scope.status = response.status;
-            }, function (response) {
-                $scope.status = response.status;
-            });
+            var data = {
+                name: $scope.master.name,
+                placeType: $scope.master.placeType,
+            };
+            if ($scope.doing == 'create') {
+                $http({
+                    url: '/posta/rest/places',
+                    method: "POST",
+                    data: data
+                }).then(function (response) {
+                    $scope.status = "New Place successfully created.";
+                    createUpdateTools.setAlerts([{type: 'success', title: 'Successfull!', msg: $scope.status}]);
+                    createUpdateTools.deleteItem();
+                    $location.path("/places");
+                }, function (response) {
+                    $scope.status = "Cannot create, "+ response.status;
+                });
+            } else {
+                $scope.messageBuilder = 'You have successfully updated these fields [';
+                $scope.updatingItem = {};
+                if (data.name != createUpdateTools.getItem().name) {
+                    $scope.updatingItem.name = data.name;
+                    $scope.messageBuilder += 'name, ';
+                }
+                if (data.placeType != createUpdateTools.getItem().placeType) {
+                    $scope.updatingItem.placeType = data.placeType;
+                    $scope.messageBuilder += 'placeType, ';
+                }
+                if ($scope.messageBuilder.includes(', ')) {
+                    $http({
+                        url: '/posta/rest/places/update/' + createUpdateTools.getItem().id,
+                        method: "POST",
+                        data: $scope.updatingItem
+                    }).then(function (response) {
+                        $scope.status = $scope.messageBuilder.substring(0, $scope.messageBuilder.length - 2) + "] of place.";
+                        createUpdateTools.setAlerts([{type: 'success', title:'Successfull!', msg: $scope.status}]);
+                        createUpdateTools.deleteItem();
+                        $location.path("/places");
+                    }, function (response) {
+                        $scope.status = "Cannot update place. An error "+ response.status +" occured.";
+                    });
+                } else {
+                    createUpdateTools.setAlerts([{type: 'info', title: "No change!", msg: 'There have been no changes.'}]);
+                    createUpdateTools.deleteItem();
+                    $location.path("/places");
+                }
+            }
         };
 
-        $scope.resetBirth = function (user) {
-            user.birth = undefined;
+        $scope.reset = function(form) {
+            if (form) {
+                form.$setPristine();
+                form.$setUntouched();
+            }
+            $scope.place = null;
+        };
+
+        $scope.removePlace = function () {
+            if($scope.place.id != null) {
+                if(confirm('You are above to completely remove Place:\n'+ $scope.place.name +'\n\nAre you sure?')) {
+                    $http({
+                        url: '/posta/rest/places/' + createUpdateTools.getItem().id,
+                        method: "DELETE"
+                    }).then(function () {
+                        createUpdateTools.setAlerts([{type: 'success', title:'Removed!', msg: 'You have successfully deleted '+ $scope.place.name +' from system.'}]);
+                        createUpdateTools.deleteItem();
+                        $location.path("/places");
+                    }, function () {
+                        createUpdateTools.setAlerts([{type: 'danger', title:'Cannot Remove!', msg: $scope.place.name +' is still assigned to some jobs.'}]);
+                        createUpdateTools.deleteItem();
+                        $location.path("/places");
+                    })
+                }
+            } else {
+                confirm('Canot remove Place immediately.\nFor removing place we need to know unique identification, which is not available now.');
+            }
         };
 
     }]);
--- front-end/src/main/webapp/elements/places/placesOverview.js
+++ front-end/src/main/webapp/elements/places/placesOverview.js
@@ -1,8 +1,29 @@
 'use strict';
 
 angular.module('angularApp')
-    .controller('PlacesCtrl', ['$scope','commonTools',function($scope, commonTools) {
+    .controller('PlacesCtrl', ['$scope', '$http', '$location', 'commonTools', 'createUpdateTools', function ($scope, $http, $location, commonTools, createUpdateTools) {
         commonTools.getPlacesAvailable().then(function (response) {
             $scope.places = response;
         });
+
+        $scope.alerts = angular.copy(createUpdateTools.getAlerts());
+        createUpdateTools.deleteAlerts();
+
+        $scope.closeAlert = function(index) {
+            $scope.alerts.splice(index, 1);
+        };
+
+
+        $scope.navigateToEdit = function (place) {
+            createUpdateTools.setItem(place);
+            $location.path('/createPlace')
+        };
+
+        $scope.createPlace = function () {
+            $scope.navigateToEdit(null);
+        };
+
+        $scope.updatePlace = function (place) {
+            $scope.navigateToEdit(place);
+        };
     }]);
